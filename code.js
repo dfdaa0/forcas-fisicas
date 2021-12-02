0cd34671773ad55c406b4bcf9196217baa00c207
Daniel Araújo <dfdaa0@outlook.com>
2021-12-02 15:46:38 -0300

    Added remaining functions and completed
    - Added some elements in the html file;
    - Changed the main paragraph (math purposes);
    - Added remaining calculating functions

--- code.js
+++ code.js
@@ -1,34 +1,39 @@
 function acao(){
-    forcaEletrica()
-    forcaGravitacional()
+    let fE = forcaEletrica()
+    let fG = forcaGravitacional()
+    let vezesMaior = fE/fG
+    let p1 = document.getElementById("forca-eletro")
+    let p2 = document.getElementById("forca-gravit")
+    let obs = document.getElementById("observacoes")
+    p1.innerText = 'A força elétrica encontrada foi ' + fE + ' Newtons'
+    p2.innerText = 'A força gravitacional encontrada foi ' + fG + ' Newtons'
+    obs.innerText = 'A força elétrica é ' + vezesMaior + ' vezes maior que a força gravitacional'
 }
 
 function forcaEletrica(){
-    const carga = 1.6 * Math.pow(10, -19) // carga do eletron em Coulombs
-    console.log('Carga do eletron em Coulombs: ' + carga)
-    const u = 1.66054 * Math.pow(10, -24) // unidade de massa atomica em Gramas
-    console.log('Massa elementar em gramas: ' + u)
-    const massaSodio = 22.989769 * u
-    console.log('Massa do átomo de sodio em gramas: ' + massaSodio)
-    const massaCloro = 35.453 * u
-    console.log('Massa do átomo de cloro em gramas: ' + massaCloro)
-    const cargaSodio = 1
-    const cargaCloro = 2
-
-    let massa1 = document.getElementById('massa1').value
-    console.log('Massa recebida do sódio em gramas: ' + massa1)
-    let massa2 = document.getElementById('massa2').value
-    console.log('Massa recebida do cloro em gramas: ' + massa2)
-
-    let cargaNa = (massa1/massaSodio)*cargaSodio
-    let cargaCl = (massa2/massaCloro)*cargaCloro
+    let ionsSodio = document.getElementById('ion1').value
+    let ionsCloro = document.getElementById('ion2').value
     let distancia = document.getElementById('distancia').value
-    console.log(cargaNa + ' elétrons extra no sódio ' + cargaCl + ' prótons extra no cloro')
-    let k = 9 * Math.pow(10, 9)
-    let forcaE = (k*cargaNa*cargaCl)/Math.pow(distancia, 2)
-    console.log('Força elétrica resultante: ' + forcaE + ' N')
+    const carga = 1.6 * Math.pow(10, -19) // carga do eletron em Coulombs
+    k = 9 * Math.pow(10, 9)
+    console.log('Carga do elétron em Coulombs: ' + carga)
+    forcaE = k*(ionsSodio*carga*ionsCloro*carga)/Math.pow(distancia, 2)
+    console.log("Força elétrica: " + forcaE + " N")
+    return forcaE
 }
 
 function forcaGravitacional(){
- //TODO
+    let ionsSodio = document.getElementById('ion1').value
+    let ionsCloro = document.getElementById('ion2').value
+    let distancia = document.getElementById('distancia').value
+    const u = 1.66054 * Math.pow(10, -24) // unidade de massa atomica em Gramas
+    console.log('Massa elementar em gramas: ' + u)
+    const massaSodio = 22.989769 * u
+    console.log('Massa do íon de sodio em gramas: ' + massaSodio)
+    const massaCloro = 35.453 * u
+    console.log('Massa do íon de cloro em gramas: ' + massaCloro)
+    const g = 6.67 * Math.pow(10, -11)
+    forcaG = g*((ionsSodio*massaSodio)*(ionsCloro*massaCloro))/Math.pow(distancia, 2)
+    console.log("Força gravitacional: " + forcaG + " N")
+    return forcaG
 }
